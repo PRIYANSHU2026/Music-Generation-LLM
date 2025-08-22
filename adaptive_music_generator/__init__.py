ff3c3b6028d6cb072be1d57d0ea2023684baad1a
PRIYANSHU TIWARI <153541511+PRIYANSHU2026@users.noreply.github.com>
2025-08-22 23:52:41 +0530

    Structure Based

---
+++ adaptive_music_generator/__init__.py
@@ -0,0 +1,7 @@
+"""Adaptive Music Generator - A professional music generation library.
+
+This package provides tools for generating adaptive music based on user parameters.
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Adaptive Music Generator Team'
